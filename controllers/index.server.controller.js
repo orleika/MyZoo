79777726311e963158b01b1178edfd384dda3e3c
Orleika <orleika.net@gmail.com>
2014-12-06 21:37:34 +0900

    [modified] index value

--- controllers/index.server.controller.js
+++ controllers/index.server.controller.js
@@ -1,6 +1,19 @@
 /**
+ * Module dependencies.
+ */
+var config = require('../config.json');
+
+/**
  * Show the index
  */
-exports.read = function(req, res) {
-  res.json(res.render('index'));
+exports.read = function (req, res) {
+  res.json(res.render('index'), {
+    title: config.title,
+    keyword: config.keyword,
+    description: config.description,
+    og_type: 'website',
+    og_image: '',
+    og_url: '',
+    og_site: ''
+  });
 };
